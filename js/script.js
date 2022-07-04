d646d64f9f959657426016d4a9fb5239da318029
Nastya <web.orlova.dev@gmail.com>
2022-07-04 18:46:06 +0300

    сделали класс-конструктор для карточек

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 window.addEventListener('DOMContentLoaded', () => {
     
 // Табы: 
@@ -119,7 +120,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
     
     // если нужно, чтобы мод.окно срабатывало спустя какое-то врмея на сайте:
-    const modalTimerId = setTimeout(openModal, 5000);
+    const modalTimerId = setTimeout(openModal, 1000000);
 
     function showModalByScroll() { // чтобы срабатыволо, когда дойдет до конца скрола 
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
@@ -128,4 +129,75 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     window.addEventListener('scroll', showModalByScroll);
+
+// Используем class для карточек:
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt; // если img не загрузиться, альтернативный текст какой-то
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector); // куда я буду пушить данный блок
+            this.transfer = 27; // это валюта грн, тк данные приходят в доллорах
+            this.changeToUAH(); 
+        }
+
+        changeToUAH() { // метод, который делает валюту в грн
+            this.price = this.price * this.transfer; 
+        }
+
+        render() { // метод, который позволяет как раз верстку запушить уже с нашими данными
+            const element = document.createElement('div'); //создаем див блок куда будем засовыйвать наш код
+
+            if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+            this.parent.append(element); //чтобы наш новосозданный объект поместили в родитель
+        }
+    }
+    // теперь нам надо вызвать все, что сверху
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();
+
+    
 });
